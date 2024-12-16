01e6010691e2e12f910a762c5f737d66717e297c
chrismartinez123987 <benjaminw.1313@gmail.com>
2024-12-16 23:44:07 +0000

    Final

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -3,35 +3,53 @@ export default function Page() {
       <>
       <h1 className="text-3xl ml-5">Final Exam </h1>
       <form>
-        <label htmlFor="product" className="ml-5">Product Description
+        <label htmlFor="FirstName" className="ml-5">First Name
 
         </label>
-        <input type="text" name="product" id="product" className="m-5 text-black 
-        bg-yellow-400 "/>
-         <br/>
-         <label htmlFor="cars">Choose a car:</label>
+        <input type="text" name="FirstName" id="FirstName" className="m-5 text-black 
+        bg-white "/>
+         <label htmlFor="LastName" className="ml-5">Last Name
 
-<select name="cars" className="mx-5" id="cars">
-  <option value="volvo">Volvo</option>
-  <option value="saab">Saab</option>
-  <option value="mercedes">Mercedes</option>
-  <option value="audi">Audi</option>
+</label>
+<input type="text" name="LastName" id="LastName" className="m-5 text-black 
+bg-white "/>
+         <br/>
+         <label htmlFor="Status">Choose your filing status:</label>
+
+<select name="Status" className="mx-5" id="Status">
+  <option value="single">Single</option>
+  <option value="marriedjointly">Married Filing Jointly</option>
+  <option value="marriedseparately">Married Filing Separately</option>
+  <option value="hoh">Head of Household</option>
+  <option value="qss">Qualifying Surviving Spouse</option>
+  <option value="alien">Nonresident Alien</option>
 </select>
 <br/>
 <div className="ml-5 mt-5"> 
-<input type="radio" id="html" name="fav_language" value="HTML"/>
-<label htmlFor="html">HTML</label><br/>
-<input type="radio" id="css" name="fav_language" value="CSS"/>
-<label htmlFor="css">CSS</label><br/>
-<input type="radio" id="javascript" name="fav_language" value="JavaScript"/>
-<label htmlFor="javascript">JavaScript</label>
+<label htmlFor="Dependency" className="ml-5">Can you be claimed as a dependent?
+</label>
+<br/>
+<input type="radio" id="yes" name="choice" value="Yes"/>
+<label htmlFor="yes">Yes</label><br/>
+<input type="radio" id="No" name="choice" value="No"/>
+<label htmlFor="No">No</label><br/>
 </div>
 <br/>
 <div className="ml-5 mt-5"> 
-<input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
-<label htmlFor="vehicle1"> I have a bike</label><br/>
-<input type="checkbox" id="vehicle2" name="vehicle2" value="Car"/>
-<label htmlFor="vehicle2"> I have a car</label>
+<input type="checkbox" id="ExtraInfo1" name="ExtraInfo1" value="Student"/>
+<label htmlFor="ExtraInfo1"> Taxpayer was over the age of 18 and a full-time student at an elgible institution</label><br/>
+<input type="checkbox" id="ExtraInfo2" name="ExtraInfo2" value="Blind"/>
+<label htmlFor="ExtraInfo2"> Taxpayer is blind</label><br/>
+<input type="checkbox" id="ExtraInfo3" name="ExtraInfo3" value="Deceased"/>
+<label htmlFor="ExtraInfo3"> Taxpayer is deceased</label><br/>
+<input type="checkbox" id="ExtraInfo4" name="ExtraInfo4" value="Campaign"/>
+<label htmlFor="ExtraInfo4"> Taxpayer wishes to contribute 3 dollars to the Presidential Election Campaign Fund</label><br/>
+<input type="checkbox" id="ExtraInfo5" name="ExtraInfo5" value="Military"/>
+<label htmlFor="ExtraInfo5"> Taxpayer or spuse serve in a combat zone during the current year</label><br/>
+<input type="checkbox" id="ExtraInfo6" name="ExtraInfo6" value="Disaster"/>
+<label htmlFor="ExtraInfo6"> Taxpayer was affected by a natural disaster during the current year</label><br/>
+<input type="checkbox" id="ExtraInfo7" name="ExtraInfo7" value="DigitalAsset"/>
+<label htmlFor="ExtraInfo7"> Taxpayer received, sold, or disposed of a digital asset in the current tax year</label><br/>
 </div>
 <br/>
 <br/>
